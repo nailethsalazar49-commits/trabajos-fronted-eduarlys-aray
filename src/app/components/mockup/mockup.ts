82db9429c9c8b468cc4aa13eb3477c73740451b6
naileth salazar <nailethsalazar@gmail.com>
2025-09-25 16:16:01 -0500

    Añadir header QUIPUX

---
+++ src/app/components/mockup/mockup.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mockup',
+  imports: [],
+  templateUrl: './mockup.html',
+  styleUrl: './mockup.css'
+})
+export class Mockup {
+
+}
